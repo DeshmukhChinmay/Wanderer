13edb430c2c61d13e08a450776e3258c6fec32da
Akrya <43310355+Akrya@users.noreply.github.com>
2020-05-24 21:40:18 +1200

    Feature/add location images using places api (#68)
    * Location images added using google places api
    Popular view changed from functional component to class to better
    support state management
    
    * uneeded code deleted
    
    Co-authored-by: Aditya Krishnan <adityakrishnan99@gmail.com>

--- client/src/components/pages/homepage/PopularView.js
+++ client/src/components/pages/homepage/PopularView.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState, useEffect} from "react";
+import testImage from "./images/testPopular.jpg";
+import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -7,12 +8,9 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import getRandomImage from "./ImageHelper";
+import axios from "axios";
 
-/**
- * Custom CSS styles for the component.
- */
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
     root: {
         width: "60%",
         height: "100%",
@@ -40,49 +38,132 @@ const useStyles = makeStyles((theme) => ({
     media: {
         height: 140,
     },
-}));
+});
 
-const placeData = [
-    {
-        img: getRandomImage(),
-        title: "Wellington",
-        description:
-            "Wellington, the capital of New Zealand, sits near the North Island’s southernmost point on the Cook Strait.",
+//Template for displaying location. Testimage added to display if api fails.
+const placeData = {
+    "Hong Kong": {
+        img: testImage,
+        title: "Country",
+        description: "An amazing city with the perfect combination of east and west. With a population of 7.4 million the city is always busting with noise.",
     },
-    {
-        img: getRandomImage(),
-        title: "Auckland",
+    "Mangere":{
+        img: testImage,
+        title: "Country",
         description:
-            "Auckland, based around 2 large harbours, is a major city in the north of New Zealand’s North Island.",
+            "The entire south auckland experience in one city. Fish and Chips, burger king everything you need.",
     },
-];
+    "London":{
+        img: testImage,
+        title: "Country",
+        description:
+            "One of the most vibrant city with a bit of everything. Perfect place to visit if you want to get colonised.",
+    },
+    "New York":{
+        img: testImage,
+        title: "Country",
+        description:
+            "It really is just like the movies. Town Square is an experience you'll never forget. And don't forget the pizzas!",
+    },
+    "Sydney":{
+        img: testImage,
+        title: "Country",
+        description:
+            "It's auckland, but better! A very diverse city with plenty of tourist attractions such as the opera house.",
+    },
+    "Paris":{
+        img: testImage,
+        title: "Country",
+        description:
+            "In this world full of sadness, all we really need is someone to love. Maybe you'll find it here.",
+    },
+};
+const GOOGLE_PLACES_API_URL = "https://maps.googleapis.com/maps/api/place";
+const GOOGLE_PLACES_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
 
 /**
  * This component will show the recommended destinations to the user.
  */
-export default function PopularView() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Typography className={classes.title} variant="h3">{`What's Popular`}</Typography>
-            {placeData.map((place, index) => (
-                <Card className={classes.card} key={index}>
-                    <CardActionArea className={classes.cardActionArea}>
-                        <CardMedia className={classes.media} image={place.img} />
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {place.title}
-                        </Typography>
-                    </CardActionArea>
-                    <CardContent className={classes.cardContent}>
-                        <Typography gutterBottom>{place.description}</Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardButtons}>
-                        <Button size="small" color="primary">
-                            {`Details`}
-                        </Button>
-                    </CardActions>
-                </Card>
-            ))}
-        </div>
-    );
+
+class PopularView extends React.Component{
+
+    //const classes = useStyles();
+
+    //hardcoded placeIDs
+    //mangere, paris, new york, hong kong, sydney, london in order
+    constructor(props){
+        super(props);
+        this.state = {
+            places: [],
+            placeIDs: ["ChIJBxpST4VPDW0R0N6iQ2HvAAU",
+            "ChIJD7fiBh9u5kcRYJSMaMOCCwQ",
+            "ChIJOwg_06VPwokRYv534QaPC8g",
+            "ChIJD5gyo-3iAzQRfMnq27qzivA",
+            "ChIJP3Sa8ziYEmsRUKgyFmh9AQM",
+            "ChIJdd4hrwug2EcRmSrV3Vo6llI"]
+        }
+    }
+    componentDidMount(){
+        const getPlaceDetails = async() =>{
+        
+            this.state.placeIDs.map((placeID, index) =>
+                {try{
+                    axios
+                    .get("/api/placeDetails/" + placeID)
+                    .then((res) => {
+                        //getting photo references and name from placeDetails api
+                        const photoReference = res.data.result.photos[0].photo_reference;
+                        let place = {};
+                        place.photoReference = photoReference;
+                        place.names = res.data.result.name;
+
+
+                        this.setState((state) => {
+                            return {
+                                places:[...state.places, place],
+                            }
+                        });             
+                    })
+                } catch (error){
+                    console.log(error)       
+                }}
+            )
+            
+                   
+        } 
+        getPlaceDetails();  
+
+    }
+    
+    render(){
+        const {classes} = this.props;
+        return (
+            <div className={classes.root}>
+                <Typography className={classes.title} variant="h3">{"Recommended Places"}</Typography>
+                {this.state.places.map((place, index) => (
+                    <Card className={classes.card} key={index}>
+                        <CardActionArea className={classes.cardActionArea}>
+                            <CardMedia className={classes.media} image={`${GOOGLE_PLACES_API_URL}/photo?maxwidth=400&photoreference=${place.photoReference}&key=${GOOGLE_PLACES_API_KEY}`}/>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {place.names}
+                            </Typography>
+                        </CardActionArea>
+                        <CardContent className={classes.cardContent}>
+                            <Typography gutterBottom>
+                                {placeData[place.names]===undefined?"":placeData[place.names].description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions className={classes.cardButtons}>
+                            <Button size="small" color="primary">
+                                {`Details`}
+                            </Button>
+                        </CardActions>
+                    </Card>
+                ))}
+            </div>
+        );
+    }
 }
+
+export default withStyles(useStyles)(PopularView);
